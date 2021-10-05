e328af0615506cc1f7478edf58e772e9bf013442
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-10-05 10:13:41 +0000

    <Naked Return, defer, Loop in GO>

--- main.go
+++ main.go
@@ -6,48 +6,31 @@ import(
 )
 
 func main() {
-  const constName string = "choi";
-  //constName = "jun" -> ERR
-  var flexibleName string = "choi";
-  flexibleName = "choi2";
-  fmt.Println("constName : "+constName,"/ flexibleName : "+flexibleName);
-
-  sayName("JunYoung");
-
-  fmt.Println(lengthOfName("Jun"));
-
-  //return couple of values 
-  var val1 int
-  var val2 string
-  var val3 string
-  //다중 return 값 전달
-  val1, val2 = multipleReturn("choi")
-  //받지않을 리턴값은 _ 로표기
-  _, val3 =  multipleReturn("choi")
-  //shortcut 이렇게 적으면 할당값에 따라 변수의 type을 자동으로 지정
-  nameLen, upperName := multipleReturn("choi")
-  fmt.Println(nameLen, upperName);
-
-  //shortcut으로 선언된 변수는 기본적으로 var형
-  nameLen = 5;
-  fmt.Println(nameLen);
-
-  fmt.Println(val1, val2);
-  fmt.Println(val3);
+  fmt.Println("Loop")
+  fmt.Println("defalutFor:", defaultFor(1,2,3,4,5))
+  fmt.Println("forEeach:", forEeachInGO(1,2,3,4,5,6,7,8))
 }
 
-func sayName(name string) { //return nothing
-  fmt.Println(name);
+//naked return function
+func PringUppercase(name string)(upperName string){
+  defer fmt.Println("I', done~") //read code after the return is finished
+  upperName = strings.ToUpper(name);
+  return
 }
 
-func lengthOfName(name string) int {
-  return len(name);
+//Loop in GO
+func defaultFor(numbers ...int) (sum int){ //numbers is out of Array
+  sum = 0
+  for i:=0; i<len(numbers); i++ {
+    sum += numbers[i]
+  }
+  return
 }
 
-func multipleReturn(name string) (int, string){
-  return len(name), strings.ToUpper(name);
-}
-
-func infiniteArg(words ...string) {
-  fmt.Println(words);
+func forEeachInGO(numbers ...int) (sum int){
+  sum = 0;
+  for _, number := range numbers{ //first return value is index of Array
+    sum += number;
+  }
+  return
 }
