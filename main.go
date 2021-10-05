d13b72cf7d5390ae810ba8395cd066b1054bf790
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-10-05 10:33:13 +0000

    <if, else, else if, switch>

--- main.go
+++ main.go
@@ -2,35 +2,29 @@ package main
 
 import(
   "fmt"
-  "strings"
+  //"strings"
 )
 
 func main() {
-  fmt.Println("Loop")
-  fmt.Println("defalutFor:", defaultFor(1,2,3,4,5))
-  fmt.Println("forEeach:", forEeachInGO(1,2,3,4,5,6,7,8))
+  fmt.Println(checkAdult(16), checkAdultSwitch(16));
 }
 
-//naked return function
-func PringUppercase(name string)(upperName string){
-  defer fmt.Println("I', done~") //read code after the return is finished
-  upperName = strings.ToUpper(name);
-  return
-}
-
-//Loop in GO
-func defaultFor(numbers ...int) (sum int){ //numbers is out of Array
-  sum = 0
-  for i:=0; i<len(numbers); i++ {
-    sum += numbers[i]
+//If else
+func checkAdult(age int) (isOk bool){
+  if koreanAge := age + 2; koreanAge < 18{
+    return false;
+  } else if koreanAge < 19{ //koreanAge는 사실상 if문 밖에서 선언한 것과 같은 효과를 낸다.
+    fmt.Println("that's close bro hahaha")
+    return true
+  } else {
+    return true
   }
-  return
 }
 
-func forEeachInGO(numbers ...int) (sum int){
-  sum = 0;
-  for _, number := range numbers{ //first return value is index of Array
-    sum += number;
+func checkAdultSwitch(age int) bool {
+  switch koreanAge := age + 2; koreanAge{
+    case 18:
+      return false
   }
-  return
+  return true 
 }
