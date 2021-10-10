8958acf311965a54b58b770ef0b94bbcad9e08a6
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-10-10 09:40:20 +0000

    <nil and error in GO>

--- main.go
+++ main.go
@@ -1,64 +1,28 @@
 package main
 
 import(
+  "errors"
   "fmt"
   //"strings"
 )
 
-type testStruct struct{
-  prop1 int
+type Person struct{
+  name string
 }
 
-func createStruct() *testStruct {  // ?? 
-  test := testStruct{};
-  fmt.Println("create함수의 test 주소 : ", &test)
-  return &test;
-}
-
-func (receiver *testStruct) testMethod(arg1 int){
-  //receiver는 관찰자로 바로 사용이 가능하다.
-  receiver.prop1 = arg1;
-  fmt.Println("&receiver :" ,&receiver)
+func (a Person) SetName(name string) error {
+  if len(name) > 5 {
+    return errors.New("To long xx");
+  }
+  return nil;
 }
 
 
+//nil is the zero value of pointers and interfaces, uninitialized pointers and interfaces will be nil.
 func main() {
-  var a = 5; //일반 변수
-  //var b* 등 에스터리스크 붙여서 변수선언 불가
-  var b = &a; //b became pointer for a 
-  fmt.Println(b) //address of a
-  fmt.Println(*b) //value of a 
-  //b = 15; b의 타입은 엄밀히 말해 *int 이다 (포인터) 따라서 일반 값으로 초기화 불가능
-  *b = 15;  //b는 a를 참조하는 포인터이다. *b로 a값에 접근해 변경이 가능
-  fmt.Println(a)
-  //포인터 활용 함수
-  add(&a, 15)
-  fmt.Println(a);
-  
-  //Struct 와 pointer
-  testinstance := createStruct();
-  fmt.Println("testinstance의 주소 : ", &testinstance)
-  testinstance.testMethod(12);
-  fmt.Println(testinstance);
-
-  testinstance2 := &testinstance;
-  (*testinstance2).testMethod(15);
-  fmt.Println(testinstance);
+  choi := Person{};
+  err := choi.SetName("choijunyoung")
+  if err != nil { 
+    fmt.Println(err)
+  }
 }
-
-func add(a *int, addition int) {
-  *a  = *a + addition;
-}
-
-/*
-0xc00018c000
-5
-15
-30
-create함수의 test 주소 :  &{0}     //
-testinstance의 주소 :  0xc000182020   
-&receiver : 0xc000182028
-&{12}
-&receiver : 0xc000182030
-&{15}
-*/
